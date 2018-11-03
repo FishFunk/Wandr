f7db2e12888cabe46118adc76a2dcf9c82569cfb
steve <go@cwcg.io>
2018-11-03 10:38:38 -0400

    interim commit

--- ionic-v2/src/pages/tabs/tabs.ts
+++ ionic-v2/src/pages/tabs/tabs.ts
@@ -10,7 +10,7 @@ export class TabsPage {
   tab1Root = ProfilePage;
   tab2Root = ProfilePage;//MessagesPage;
   tab3Root = ProfilePage;//ExplorePage;
-  tab4Root =ProfilePage;// InvitationPage;
+  tab4Root = ProfilePage;// InvitationPage;
   tab5Root = ProfilePage;//HelpPage;
 
   constructor() {
