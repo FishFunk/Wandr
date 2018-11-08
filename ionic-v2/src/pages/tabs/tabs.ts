c80c4e8fa2a772fa211ea42d520b0471b62f112c
Daniel Fishman <fishfry62@gmail.com>
2018-11-07 18:14:10 -0800

    Updated styling for app tabs

--- ionic-v2/src/pages/tabs/tabs.ts
+++ ionic-v2/src/pages/tabs/tabs.ts
@@ -1,22 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { ProfilePage } from '../profile/profile';
 import { InboxPage } from '../messages/inbox';
 import { InvitePage } from '../invite/invite';
 import { MapPage } from '../explore/map';
-
+import { Tabs } from 'ionic-angular';
 
 
 @Component({
+  selector: 'tabs-page',
   templateUrl: 'tabs.html'
 })
 
 export class TabsPage {
+
+  @ViewChild('appTabs') tabRef: Tabs;
+
+  ionViewDidEnter() {
+    // Select first tab to be shown
+ }
+
   tab1Root = ProfilePage;
   tab2Root = InboxPage;
   tab3Root = MapPage;
   tab4Root = InvitePage;
-  tab5Root = ProfilePage;//HelpPage;
+  tab5Root = ProfilePage; //HelpPage;
 
-  constructor() {
+  constructor(){
+  }
+
+  onClickExploreButton(){
+    this.tabRef.select(2);
   }
 }
