8b95401c53b13ab6f5af6f7a29716dbf68c19f50
Daniel Fishman <fishfry62@gmail.com>
2018-12-28 21:27:21 -0800

    converted to use firestore

--- firecast/functions/src/index.ts
+++ firecast/functions/src/index.ts
@@ -1,20 +1,50 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+import { user } from 'firebase-functions/lib/providers/auth';
 
 admin.initializeApp();
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
+
+// exports.newUserNotification = 
+//     functions.firestore.document('users/{userId}')
+//     .onCreate(async event =>{
+
+//         const newUser = event.data();
+//         const userId = newUser.app_uid;
+//         const userName = newUser.first_name;
+//         const userLoc = newUser.location.stringFormat;
+//         const friendsToNotify = newUser.friends;
+
+//         const msg = userLoc ? 
+//             `${userName} has joined your network in ${userLoc}!` :
+//             `${userName} is now in your network!`
+
+//         const payload = {
+//             notification: {
+//                 title: "You Have a New Connection!",
+//                 body: msg
+//             }
+//         }
+
+//         const db = admin.firestore();
+
+//     });
+
+
+// exports.newMessageNotification = 
+//     functions.firestore.document('messages/{timestamp}')
+//     .onCreate(async event =>{
+//         const newMessage = event.data();
+//     });
 
 // Saves a message to the Firebase Realtime Database but sanitizes the text by removing swearwords.
 exports.createChat = functions.https.onCall((chatData, context) => {
 
     chatData.lastMessage = `Hey ${chatData.userB_name}! Looks like you and ${chatData.userA_name} have a network connection! Why don't you get to know each other?\n\n- Travel Guru Bot`;
-    
+
     return _createChatRoom(chatData)
         .then(()=>{
             return { status: 'chat-created' };
@@ -25,26 +55,25 @@ exports.createChat = functions.https.onCall((chatData, context) => {
 });
 
 async function _createChatRoom(chatData: any){
-    const snapshot = await admin.database().ref('/chats/' + chatData.roomkey).once('value')
+    const docRef = await admin.firestore().collection('chats').doc(chatData.roomkey).get()
         .catch((error)=>{
+            console.error(error);
             throw new functions.https.HttpsError('internal', 'Fetching chats/{roomkey}', error);
         });
 
-    if(snapshot.val()){
+    if(docRef.exists){
         // Chat already exists
         throw new functions.https.HttpsError('already-exists');
     } else {
         // Create new chat room
-        await admin.database().ref('/chats/' + chatData.roomkey).set(chatData)
+        await admin.firestore().collection('chats').doc(chatData.roomkey).set(chatData)
             .catch((error)=> {
+                console.error(error);
                 throw new functions.https.HttpsError('internal', 'Setting chats/{roomkey}', error);
             });
         
         // Give users roomkey
-        await _updateUsersWithRoomkey(chatData.userA_id, chatData.userB_id, chatData.roomkey)
-            .catch(error =>{
-                throw new functions.https.HttpsError('internal', 'Assigning roomkey to users', error);
-            })
+        await _updateUsersWithRoomkey(chatData.userA_id, chatData.userB_id, chatData.roomkey);
 
         // Send first message in chat
         return _createAndSendFirstMessage(chatData);
@@ -53,48 +82,105 @@ async function _createChatRoom(chatData: any){
 
 async function _updateUsersWithRoomkey(userA_id, userB_id, roomkey){
 
-    const userIds = [userA_id, userB_id];
+    if(!userA_id || !userB_id || !roomkey){
+        console.error("_updateUsersWithRoomkey() - Invalid params");
+        throw new functions.https.HttpsError(
+            'internal', 
+            '_updateUsersWithRoomkey Invalid Params', 
+            '_updateUsersWithRoomkey Invalid Params');
+    }
 
-    const promises = userIds.map((id)=> {
-        return admin.database().ref("/users/").child(id).once("value");
-    });
-
-    const snapshots = await Promise.all(promises)
-        .catch((error)=> {
-            throw new functions.https.HttpsError('internal', 'Fetching users by ID', error);
+    const userA_docRef = await admin.firestore().collection('users').doc(userA_id).get()
+        .catch((error)=>{
+            console.error(error);
+            throw new functions.https.HttpsError(
+                'internal', 
+                'Fetching user A by ID', 
+                'Failed to read user A by ID');
         });
 
-    let updates = {};
-    snapshots.forEach(snap=>{
-        let user = snap.val();
-        if(user.roomkeys){
-            user.roomkeys.push(roomkey);
-        } else {
-            user.roomkeys = [roomkey];
-        }
+    const userB_docRef = await admin.firestore().collection('users').doc(userB_id).get()
+        .catch((error)=>{
+            console.error(error);
+            throw new functions.https.HttpsError(
+                'internal', 
+                'Fetching user B by ID', 
+                'Failed to read user B by ID');
+        });
 
-        updates['/users/' + user.app_uid] = user;
-    });
+    if(!userA_docRef.exists){
+        console.error("_updateUsersWithRoomkey() - User A doc doesn't exist");
+        throw new functions.https.HttpsError(
+            'internal', 
+            'Fetching user B by ID', 
+            'Failed to read user B by ID');
+    }
 
-    return admin.database().ref().update(updates)
-        .catch(error =>{
-            throw new functions.https.HttpsError('internal', 'Updating user roomkeys', error);
+    if(!userB_docRef.exists){
+        console.error("_updateUsersWithRoomkey() - User B doc doesn't exist");
+        throw new functions.https.HttpsError(
+            'internal', 
+            'Fetching user B by ID', 
+            'Failed to read user B by ID');
+    }
+        
+    const userA = userA_docRef.data();
+    const userB = userB_docRef.data();
+
+    userA.roomkeys = userA.roomkeys ? userA.roomkeys.concat([roomkey]) : [roomkey];
+    userB.roomkeys = userB.roomkeys ? userB.roomkeys.concat([roomkey]) : [roomkey];
+
+    await admin.firestore().collection('users').doc(userA_id).update({
+            roomkeys: userA.roomkeys
+        })
+        .catch(error=>{
+            console.error(error);
+            throw new functions.https.HttpsError('internal', 'Updating user A roomkeys', error);
+        });
+
+    await admin.firestore().collection('users').doc(userB_id).update({
+            roomkeys: userB.roomkeys
+        })
+        .catch(error=>{
+            console.error(error);
+            throw new functions.https.HttpsError('internal', 'Updating user B roomkeys', error);
         });
 }
 
 async function _createAndSendFirstMessage(chatData: any){
-    const snapshot = await admin.database().ref('/messages/' + chatData.roomkey).once('value');
-    if(snapshot.val()){
-        throw new functions.https.HttpsError('already-exists');
-    } else {
-        return admin.database().ref('/messages/' + chatData.roomkey).child(chatData.timestamp).set({
-            uid: 'travel_guru_bot',
-            name: 'Travel Guru Bot',
-            text: chatData.lastMessage,
-            timestamp: chatData.timestamp
-        })
-        .catch(error =>{
-            throw new functions.https.HttpsError('internal', 'Setting messages/{roomkey}/{timestamp}', error); 
+    const docRef = await admin.firestore().collection('messages').doc(chatData.roomkey).get()
+        .catch(error=>{
+            console.error(error);
+            throw new functions.https.HttpsError('internal', 'Fetching messages/{roomkey}', error);
         });
+
+    if(docRef.exists){
+        console.warn("_createAndSendFirstMessage - attempted to create duplicate message");
+        throw new functions.https.HttpsError(
+            'already-exists', 
+            '_createAndSendFirstMessage', 
+            '_createAndSendFirstMessage');
     }
+
+    const data = {};
+    data[chatData.timestamp] = {
+        to_uid: '',
+        from_uid: 'travel_guru_bot',
+        name: 'Travel Guru Bot',
+        text: chatData.lastMessage,
+        timestamp: chatData.timestamp
+    };
+    
+    return admin.firestore()
+        .collection('messages')
+        .doc(chatData.roomkey)
+        .set(data)
+        .catch(error=>{
+            console.error(error);
+            throw new functions.https.HttpsError(
+                'internal', 
+                'Setting messages/{roomkey}/{timestamp}', 
+                error); 
+        });
+    
 }
