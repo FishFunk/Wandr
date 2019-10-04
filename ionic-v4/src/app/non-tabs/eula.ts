f7d84a6d3c5e24cf4e7ae5c22b67dbe178eebebe
Daniel Fishman <fishfry62@gmail.com>
2019-10-03 18:29:30 -0700

    EULA is dynamically loaded from firebase

--- ionic-v4/src/app/non-tabs/eula.ts
+++ ionic-v4/src/app/non-tabs/eula.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { AngularFirestore } from 'angularfire2/firestore';
  
 @Component({
   templateUrl: 'eula.html',
@@ -7,9 +8,18 @@ import { ModalController } from '@ionic/angular';
 })
 
 export class EulaModal {
-  constructor(private modalCtrl: ModalController){
+  eula: any = "";
+
+  constructor(private modalCtrl: ModalController,
+    private angularFirestore: AngularFirestore){
+      this.load();
   }
 
+  async load(){
+    var snapshot = await this.angularFirestore.collection('metadata').doc('terms_html').get().toPromise();
+    var metadata = snapshot.data();
+    this.eula = metadata.data;
+  }
 
   onClickClose(){
     this.modalCtrl.dismiss();
